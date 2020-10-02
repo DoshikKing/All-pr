f9417ecf46fa27bfea38afaf6dbd8dcd7b19c4c5
Konstantin <KonstantinMagin@yandex.ru>
2020-10-02 09:45:50 +0300

    +8.1-8.2-8.3

---
+++ Pr8/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    //8.1
+	//ShapeCreation shape = new ShapeCreation();
+
+    //8.2
+        /*
+    System.out.println("Введите путь...");
+    String path = new String();
+    Scanner sc = new Scanner(System.in);
+    path = sc.nextLine();
+    new ShowImage(path);
+*/
+    Img img = new Img();
+    }
+}
