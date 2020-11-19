05d885ca935b899547d561bdbd23c1f8ada72992
Konstantin <KonstantinMagin@yandex.ru>
2020-11-19 21:13:25 +0300

    +21. Files: Exep1.java and Exep1execute.java. and Answer.txt.

---
+++ Pr_from_15_to_new/src/com/company4/Exep1.java
@@ -0,0 +1,8 @@
+package com.company4;
+
+public class Exep1 {
+    void excepDemo(){
+        double n  =2.0/0.0;
+        System.out.println(n);
+    }
+}
---
+++ Pr_from_15_to_new/src/com/company4/Exep1execute.java
@@ -0,0 +1,8 @@
+package com.company4;
+
+public class Exep1execute {
+    public static void main(String[] args) {
+        Exep1 exception1=new Exep1();
+        exception1.excepDemo();
+    }
+}
